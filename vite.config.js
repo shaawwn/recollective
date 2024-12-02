c8f03e24b88dbbc442701190a62548aa4dbf1a84
Shawn Lentz <shawn.lentz@gmail.com>
2024-12-02 06:47:42 -1000

    .

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/recollective',
+  base: '/recollective/',
   plugins: [react()],
   test: {
     globals: true,  // This makes 'expect' globally available
