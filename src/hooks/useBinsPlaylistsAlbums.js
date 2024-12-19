c90fa38f4b09046573473b1447cae8baa9d81f57
Shawn Lentz <shawn.lentz@gmail.com>
2024-12-19 07:52:33 -1000

    .

--- src/hooks/useBinsPlaylistsAlbums.js
+++ src/hooks/useBinsPlaylistsAlbums.js
@@ -10,9 +10,9 @@ export default function useBinsPlaylistsAlbums() {
     const {accessToken} = useAuthContext() || {}
     const {user} = useUserContext() || {}
     const {spotifyApi, recollectiveApi} = useApiContext() || {}
-    const [bins, setBins] = useState([])
-    const [playlists, setPlaylists] = useState([])
-    const [albums, setAlbums] = useState([])
+    const [bins, setBins] = useState()
+    const [playlists, setPlaylists] = useState()
+    const [albums, setAlbums] = useState()
 
     // next urls for paginated results
     const nextPlaylists = useRef()
